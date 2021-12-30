aeb5dd1c32d4ac8e1b5f52be453cea93ff67e9b3
vj_62 <vj_62@DESKTOP-ES4EHF5>
2021-12-30 13:51:24 +0000

    Segunda version Kata4

--- src/Controler/Kata4.java
+++ src/Controler/Kata4.java
@@ -1,14 +1,41 @@
 package Controler;
 
+import java.util.*;
+import Model.*;
+import View.*;
+
 public class Kata4 {
 
 
+    MailListReader mailListReader;
+    List<Mail> lista;
+    MailHistogramBuilder mailHistogramBuilder;
+    Histogram <String> histogram;
+
     public static void main(String[] args) throws Exception{
         String fileName = "email.txt";
-        View.MailListReader m = new View.MailListReader();
-        View.MailHistogramBuilder b = new View.MailHistogramBuilder();
-        View.HistogramDisplay d = new View.HistogramDisplay(b.build(m.read(fileName)));
+        MailListReader m = new MailListReader();
+        MailHistogramBuilder b = new MailHistogramBuilder();
+        HistogramDisplay d = new HistogramDisplay(b.build(m.read(fileName)));
         d.execute();
+        Kata4 k1 = new Kata4();
+        k1.execute();
+    }
+    public void execute()throws Exception{
+        input();
+        process();
+        output();
+    }
+    protected void input()throws Exception{
+        mailListReader = new MailListReader();
+        lista = mailListReader.read("email.txt");
+        mailHistogramBuilder = new MailHistogramBuilder();
+    }
+    protected void process(){
+        histogram = mailHistogramBuilder.build(lista);
+    }
+    protected void output(){
+        new HistogramDisplay(histogram).execute();
     }
 
 }
